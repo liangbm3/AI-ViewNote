5d62343e3e0c8a83f78b31208b870c35b4c1998b
bemon.liang <3095088766@qq.com>
2026-03-20 12:52:26 +0800

    feat: 项目重新初始化

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wails from "@wailsio/runtime/plugins/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [react(), wails("./bindings")],
+});
